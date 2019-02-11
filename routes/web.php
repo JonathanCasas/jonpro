dccb482b0cea092f4aaf442a342bda8dc799f811
Jonathan <joncasasq@gmail.com>
2019-02-10 22:13:30 -0500

    Se quitan las rutas de instalación

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,3 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('settings', 'SettingsController@create')->name('settings.create');
     Route::post('settings', 'SettingsController@store')->name('settings.store');
 });
-Route::get('install', function () {
-    Artisan::call('migrate:refresh');
-    Artisan::call('db:seed');
-});
